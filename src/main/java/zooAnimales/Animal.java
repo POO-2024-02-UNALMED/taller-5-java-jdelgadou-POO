6e921cd88d4201b3683705f53012f887028a3788
jdelgadou-POO <jdelgadou@unal.edu.co>
2024-12-08 17:29:22 -0500

    Prueba9

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -26,11 +26,11 @@ public class Animal {
     }
 
     public static String totalPorTipo(){
-        return "Mamiferos" + Mamifero.cantidadMamiferos() + "\n"+
-        "Aves" + Ave.cantidadAves() + "\n"+
-        "Reptiles" + Reptil.cantidadReptiles() + "\n"+
-        "Peces" + Pez.cantidadPeces() + "\n"+
-        "Anfibios" + Anfibio.cantidadAnfibios() + "\n";
+        return "Mamiferos" + Mamifero.cantidadMamiferos()+1 + "\n"+
+        "Aves" + Ave.cantidadAves()+1 + "\n"+
+        "Reptiles" + Reptil.cantidadReptiles()+1 + "\n"+
+        "Peces" + Pez.cantidadPeces()+1 + "\n"+
+        "Anfibios" + Anfibio.cantidadAnfibios()+1 + "\n";
     }
     
     public String toString(){
